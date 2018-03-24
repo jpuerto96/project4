63d1678801928e968bf4f1b42129434119c970d6
s8erdude <jpuerto96@hotmail.com>
2018-03-24 17:22:10 -0400

    initial commit boooyyyy

---
+++ project4252/config.h
@@ -0,0 +1,32 @@
+/*********************************************************
+ * config.h - Configuration data for the driver.c program.
+ *********************************************************/
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+/* 
+ * CPEs for the baseline (naive) version of the rotate function that
+ * was handed out to the students. Rd is the measured CPE for a dxd
+ * image. Run the driver.c program on your system to get these
+ * numbers.  
+ */
+#define R64    3.3
+#define R128   4.8
+#define R256   7.0
+#define R512   11.8
+#define R1024  14.4
+
+/* 
+ * CPEs for the baseline (naive) version of the smooth function that
+ * was handed out to the students. Sd is the measure CPE for a dxd
+ * image. Run the driver.c program on your system to get these
+ * numbers.  
+ */
+#define S32   76.2
+#define S64   77.2
+#define S128  77.4
+#define S256  77.6
+#define S512  77.7
+
+
+#endif /* _CONFIG_H_ */
